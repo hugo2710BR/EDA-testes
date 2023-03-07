19da35020b6df4a53f3530be21266973fd86d81b
hugoBR2710 <hugosoares2710@outlook.pt>
2023-03-07 11:56:38 +0000

    Criação de array com apontadores

---
+++ header.h
@@ -0,0 +1,7 @@
+#ifndef BUSCAVALOR_H
+#define BUSCAVALOR_H
+
+int BuscaValor(int size);
+
+#endif
+
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "header.h"
+
+int main() {
+    int size = 10;
+
+    BuscaValor(size);
+
+    return 0;
+}
