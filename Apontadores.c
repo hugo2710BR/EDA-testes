3c7876cb50a39f74a53a87b7b6c66c55f8f5417f
hugoBR2710 <hugosoares2710@outlook.pt>
2023-03-10 18:11:30 +0000

    Apontadores Inicio

--- Apontadores.c
+++ Apontadores.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main (){
+    int x = 10;
+    int* ptr = &x;
+
+    *ptr = 20; // sempre que quiser chamar um apontador tenho que usar o *
+              // Neste caso, utilizei o pointer ptr para substituir o valor de x
+
+    printf("Valor de x: %d\n", x);
+    printf("Valor apontado por ptr: %d\n", *ptr);
+
+
+}
